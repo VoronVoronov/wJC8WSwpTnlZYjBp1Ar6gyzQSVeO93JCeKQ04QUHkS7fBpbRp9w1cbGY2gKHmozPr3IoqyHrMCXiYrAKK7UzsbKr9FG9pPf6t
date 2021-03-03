180febfb43b5225bbb790296ec8e24b8808a03d8
Walkerstech <59320246+Walkerstech@users.noreply.github.com>
2021-03-03 04:25:47 +0300

    Added WebmoneyP Redirect

--- theme/payment/webmoneyp.zara.php
+++ theme/payment/webmoneyp.zara.php
@@ -11,6 +11,7 @@
     <p>
         <input type="hidden" name="LMI_PAYMENT_AMOUNT" value="{{ $sum }}">
         <input type="hidden" name="LMI_PAYMENT_DESC" value="Оплата счета №{{ $payment['donation_id'] }}">
+        <input type="hidden" name="LMI_PAYMENT_DESC_BASE64" value="{{ $desc }}">
         <input type="hidden" name="LMI_PAYMENT_NO" value="{{ $payment['donation_id'] }}">
         <input type="hidden" name="LMI_PAYEE_PURSE" value="{{ $webmoneyp }}">
         <input type="hidden" name="LMI_PAYMENTFORM_SIGN" value="{{ hash('sha256', $webmoneyp.';'.$sum.';'.$payment['donation_id'].';UoPyhd5I7XI2WSuvPIBkHVI1;') }}">
