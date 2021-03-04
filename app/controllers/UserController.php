8ad2ea0e46cf588009d3acbcf7e05dd892168bcc
Walkerstech <59320246+Walkerstech@users.noreply.github.com>
2021-03-05 00:31:46 +0300

    Add connect vk

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -752,11 +752,12 @@ class UserController extends Controller {
                     $userInfo = $userInfo['response'][0];
                     if(!($user = $this->UserModel->getUser($userInfo['id'], "user_vk"))) {
                         $this->UserModel->editUser(session("user_id"), ['user_vk' => $userInfo['id']]);
-                        return view("user/profile");
-                        //return json_encode(['status' => "success"]);
+                        $result = ['status' => "success"];
+                        return view("user/profile", json_encode($result));
                     }else {
-                        return view("user/profile");
-                        //return json_encode(["status" => "error", "error" => "Данная страница VK уже привязана."]);
+                        //redirect(route("user.profile"));
+                        $result = ["status" => "error", "error" => "Данная страница VK уже привязана."];
+                        return view("user/profile", json_encode($result));
                     }
 
                 }
