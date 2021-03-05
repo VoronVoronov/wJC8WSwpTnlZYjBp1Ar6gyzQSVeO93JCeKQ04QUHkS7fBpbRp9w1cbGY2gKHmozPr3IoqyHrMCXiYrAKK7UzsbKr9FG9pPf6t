f8fefa21073f3f160899617b6c411f5f60c6decb
Walkerstech <59320246+Walkerstech@users.noreply.github.com>
2021-03-05 20:51:45 +0300

    05.03.2021 20:51

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -506,7 +506,7 @@ class UserController extends Controller {
             $tokenInfo = json_decode($result, true);
             //dd($tokenInfo);
             if(isset($tokenInfo['access_token'])) {
-                $ch1 = curl_init('https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&mine=true&access_token='.$tokenInfo['access_token'].'&key=AIzaSyCU5VqW4A7siaifEj05lmA_9HUyFpnb4UA');
+                $ch1 = curl_init('https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&mine=true&key=AIzaSyCU5VqW4A7siaifEj05lmA_9HUyFpnb4UA');
                 curl_setopt($ch1, CURLOPT_RETURNTRANSFER, true);
                 curl_setopt($ch1, CURLOPT_HTTPHEADER, array(
                     'Authorization: Bearer ' . $tokenInfo['access_token'],
