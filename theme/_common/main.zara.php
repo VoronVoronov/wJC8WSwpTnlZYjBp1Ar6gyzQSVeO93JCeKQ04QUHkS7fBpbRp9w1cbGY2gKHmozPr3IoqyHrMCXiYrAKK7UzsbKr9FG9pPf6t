eae052c54726d74d7b3965f6a9f57308bb8a0709
Walkerstech <59320246+Walkerstech@users.noreply.github.com>
2021-03-07 09:28:30 +0300

    Deleted VK widgets

--- theme/_common/main.zara.php
+++ theme/_common/main.zara.php
@@ -106,7 +106,7 @@
 
 <div id="vk_community_messages"></div>
 <script type="text/javascript">
-    VK.Widgets.CommunityMessages("vk_community_messages", 190202028, {tooltipButtonText: "Здравствуйте,чем могу вам помочь?"});
+    VK.Widgets.CommunityMessages("vk_community_messages", 174659405, {tooltipButtonText: "Здравствуйте,чем могу вам помочь?"});
 </script>
 <script>
     var tour = new Tour({
