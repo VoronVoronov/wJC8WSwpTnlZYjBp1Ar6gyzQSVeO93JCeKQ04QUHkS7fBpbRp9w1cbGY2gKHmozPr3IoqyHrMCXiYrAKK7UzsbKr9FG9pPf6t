aac47092d6e12c76f4c2866f37e4d0cf16c65504
Walkerstech <59320246+Walkerstech@users.noreply.github.com>
2021-03-08 04:41:07 +0300

    Testing

--- app/routers.php
+++ app/routers.php
@@ -128,5 +128,6 @@ Router::get("webmoney/handler", "PaymentController@WebmoneyHandler");
 Router::get("paypal/handler", "PaymentController@PaypalHandler");
 
 //Testing
+Router::get("qiwi/handlertest", "PaymentController@QiwiTestHandler");
 Router::post("qiwi/handlertest", "PaymentController@QiwiTestHandler");
 
